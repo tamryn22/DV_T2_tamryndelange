b95de5170cab402c2cbd84af544e50d38d127cb8
tamryn22 <125568338+tamryn22@users.noreply.github.com>
2023-06-05 10:09:02 +0200

    changed form and styling

--- js/build.js
+++ js/build.js
@@ -1,40 +1,109 @@
-addStudent = () => {
+addOrder= () => {
     let first = document.getElementById("first").value
-    let last = document.getElementById("last").value 
+    let location = document.getElementById("location").value 
     
-    let subject =document.getElementById("subject").value
-    //console.log(first, last, age, subject)
-    // if(age >= 18){
-    //     alert("The Student , "+ first + " " + last + " has been added to " + subject)
-    // }else{
-    //     alert("The student is not old enough to enroll")
-    // }
+    let payment =document.getElementById("payment").value
+    console.log(first, location, payment)
+    
 }
 //number add a + before the doc.get
+let subForm = [];
 
-makePizza = () => {
-    let pizzaName = document.getElementById("pizzaName").value
+makeSub = () => {
+
+    let subTotal = 0;
+
+    let subName = document.getElementById("name").value
     let size = document.getElementById("size").value
     
-    //for checking what is checked 
-    let baseOptions = document.getElementsByName("baseRadio")
-    //console.log(baseOptions)
-    let badeOptions = document.getElementsByName("baseRadio")
-    let baseValue ;
-    for(let i = 0; i < baseOptions.length; i++){
-        if(baseOptions [i].checked){
-            baseValue = baseOptions [i].value
+    if(size === "Small"){
+        subTotal = subTotal + 15;
+    } else if(size === "Medium"){
+        subTotal = subTotal + 30;
+    } else if(size === "Large"){
+        subTotal = subTotal + 50;
+    }
+    
+    let fillingsOptions = document.getElementsByName("fillings");
+    let topArray = [];
+    for(let i = 0; i < fillingsOptions.length; i++){
+        if(fillingsOptions[i].checked){
+            topArray.push(fillingsOptions[i].value);
+            subTotal = subTotal + +fillingsOptions[i].dataset.cost
         }
     }
-     //console.log(baseValue)
-    let salami = document.getElementById("salami").checked
-    if(salami){
-        alert("Pizza Order: The " + pizzaName + " - Size" + size + " - Base" + baseValue + ", with Salami")
-    } else{
-        alert("Pizza Order: The " + pizzaName + " - Size" + size + " - Base" + baseValue + ", without Salami")
-    
-    }
 
+    pizzaOrder.push({
+        subName: subName,
+        Size: size,
+        subFillings: topArray,
+        subPrice: subTotal
+    });
 
+    console.log(subOrder)
+    document.getElementById("realTimeCost").innerHTML = "R0.00"
+    document.getElementById("subForm").reset();
 
 }
+
+realTimeCost = () => {
+
+    realTimeValue = 0;
+    let size = document.getElementById("size").value;
+
+    if(size === "Small"){
+        realTimeValue = realTimeValue + 15;
+    } else if(size === "Medium"){
+        realTimeValue = realTimeValue + 30;
+    } else if(size === "Large"){
+        realTimeValue = realTimeValue + 50;
+    }
+
+    let fillingsOptions = document.getElementsByName("fillings");
+    for(let i = 0; i < fillingsOptions.length; i++){
+        if(fillingsOptions[i].checked){
+            realTimeValue = realTimeValue+ +fillingsOptions[i].dataset.cost
+        }
+    }
+    // console.log (realTimeValue)
+
+    document.getElementById("realTimeCost").innerHTML = "R" + realTimeValue + ". 00"
+    
+}
+
+displayOrder = () => {
+    //decoupling- request and response 
+       let area = document.getElementById ("orders");
+       let total = document.getElementById("orderTotal");
+   
+      area.innerHTML ="";
+   
+       let overallTotal = 0;
+       for(let i = 0; i < subOrder.length; i ++){
+   
+           let name = subOrder [i] .subName;
+           let size = subOrder [i] .size;
+           let fillings = subOrder [i] .fillingsOptions;
+           let cost = subOrder [i] .subPrice;
+   
+           overallTotal += cost;
+   // + for insert
+           area.innerHTML += `
+           
+           <div class="card" style="width: 18rem;">
+               <img src="/images/side-view-sandwich-white-bread-with-grilled-meat-cutlet-cheese-lettuce-french-fries-mayo-ketchup-boardjpg.jpg" class="card-img-top" alt="YOUR PIZZA">
+               <div class="card-header">
+                   <h3 class="card-title">${name}</h3> 
+               </div>
+               <ul class="list-group list-group-flush">
+                   <li class="list-group-item"><strong>Size:</strong> ${size}</p></li>
+                   <li  class="list-group-item"><strong>Toppings:</strong>${fillings.join(", ")}</p></li>
+                   <li class="list-group-item"><strong>R${cost}.00</strong></p></li>
+               </ul>
+           </div>`
+   
+           total.innerHTML =" R" + overallTotal + ".00";
+       }
+   
+   
+   }
--- js/checkout.js
+++ js/checkout.js
@@ -3,9 +3,72 @@ calculate = () => {
     let valueTwo = +document.getElementById("valueTwo").value 
     let operator =document.getElementById("operator").value
     console.log(valueOne, valueTwo, operator)
-    // if(age >= 18){
-    //     alert("The Student , "+ first + " " + last + " has been added to " + subject)
-    // }else{
-    //     alert("The student is not old enough to enroll")
-    // }
+    
+}
+
+realTimeCost = () => {
+
+    realTimeValue = 0;
+    let size = document.getElementById("size").value;
+
+    if(size === "Small"){
+        realTimeValue = realTimeValue + 20;
+    } else if(size === "Medium"){
+        realTimeValue = realTimeValue + 40;
+    } else if(size === "Large"){
+        realTimeValue = realTimeValue + 60;
+    }
+
+    
+
+    let toppingOptions = document.getElementsByName("toppings");
+    for(let i = 0; i < toppingOptions.length; i++){
+        if(toppingOptions[i].checked){
+            realTimeValue = realTimeValue+ +toppingOptions[i].dataset.cost
+        }
+    }
+    // console.log (realTimeValue)
+
+    document.getElementById("realTimeCost").innerHTML = "R" + realTimeValue + ". 00"
+    
+}
+
+
+displayOrder = () => {
+ //decoupling- request and response 
+    let area = document.getElementById ("orders");
+    let total = document.getElementById("orderTotal");
+
+   area.innerHTML ="";
+
+    let overallTotal = 0;
+    for(let i = 0; i < pizzaOrder.length; i ++){
+
+        let name = pizzaOrder [i] .pizzaName;
+        let size = pizzaOrder [i] .pizzaSize;
+        let base = pizzaOrder [i] .pizzaBase;
+        let toppings = pizzaOrder [i] .pizzaToppings;
+        let cost = pizzaOrder [i] .pizzaPrice;
+
+        overallTotal += cost;
+// + for insert
+        area.innerHTML += `
+        
+        <div class="card" style="width: 18rem;">
+            <img src="/images/side-view-sandwich-white-bread-with-grilled-meat-cutlet-cheese-lettuce-french-fries-mayo-ketchup-boardjpg.jpg" class="card-img-top" alt="YOUR PIZZA">
+            <div class="card-header">
+                <h3 class="card-title">${name}</h3> 
+            </div>
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item"><strong>Base:</strong> ${base}</p></li>
+                <li class="list-group-item"><strong>Size:</strong> ${size}</p></li>
+                <li  class="list-group-item"><strong>Toppings:</strong>${toppings.join(", ")}</p></li>
+                <li class="list-group-item"><strong>R${cost}.00</strong></p></li>
+            </ul>
+        </div>`
+
+        total.innerHTML =" R" + overallTotal + ".00";
+    }
+
+
 }
